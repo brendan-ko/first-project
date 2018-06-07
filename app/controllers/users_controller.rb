568b1e97e3bc75881879fb7d3b5b9c9ddf964f22
Brendan Ko <brendan.ko@plated.com>
2018-06-07 14:38:20 -0400

    Add show controller and view for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
-  
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render :show
+    else
+      render json: ["User does not exist"], status: 404
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
